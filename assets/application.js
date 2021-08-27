37fb995ff7eff7a0062581e54aea2ab4e9a06c22
sherif <sherifkhadr0@gmail.com>
2021-08-27 16:06:51 +0200

    first commit

---
+++ assets/application.js
@@ -0,0 +1,9 @@
+
+if(document.querySelector('#sort_by') != null)
+{
+document.querySelector('#sort_by').addEventListener('change',function(e){
+    var url = new URL(window.location.href);
+    url.searchParams.set('sort_by',e.currentTarget.value);
+    window.location=url.href;
+});
+}
